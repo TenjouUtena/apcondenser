d455d1055319462baaf39e3b2203f9e382e952c3
Zach Parks <zach@alliware.com>
2024-11-20 11:37:15 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+A silly webpage that allows someone to log into multiple rooms and output the
+message contents to a central "chat". Good for streams.
+
+Code is probably pretty sus.
+
+to install: `npm install`
+
+to run: `npm run dev`
+
+to build: `npm run build`
+
+may need to edit the vite.config.js file if putting on a web server, i only made
+sure it worked for my test webhost.
+
+Uses archipelago.js (which i made yey). Check it out [here](https://archipelago.js.org).
---
+++ main.js
@@ -0,0 +1,105 @@
+import "./style.css"
+import { Client } from "archipelago.js";
+
+const chat = document.getElementById("chat");
+
+const clients = [new Client(), new Client()];
+
+// supports an arbitrary amount of clients, just add more above and add appropriate inputs to HTML.
+for (let i = 0; i < clients.length; i++) {
+    const client = clients[i];
+    const identifier = `room${i + 1}`;
+
+    const hostname = document.getElementById(`${identifier}-hostname`);
+    const username = document.getElementById(`${identifier}-username`);
+    const password = document.getElementById(`${identifier}-password`);
+    const connect = document.getElementById(`${identifier}-connect`);
+
+    connect.addEventListener("click", () => {
+        connect.disabled = true;
+        hostname.disabled = true;
+        username.disabled = true;
+        password.disabled = true;
+
+        client.login(hostname.value, username.value, "", { password: password.value, tags: ["TextOnly"] })
+            .then(() => {
+                const element = document.createElement("span");
+                element.innerText = `${client.name} connected as room: ${identifier}.`;
+                addChat(element);
+            })
+            .catch((error) => {
+                alert(error);
+                connect.disabled = false;
+                hostname.disabled = false;
+                username.disabled = false;
+                password.disabled = false;
+            })
+    });
+
+    client.socket.on("disconnected", () => {
+        const element = document.createElement("span");
+        element.innerText = `${client.name} lost connection to ${identifier}.`;
+        addChat(element);
+
+        connect.disabled = false;
+        hostname.disabled = false;
+        username.disabled = false;
+        password.disabled = false;
+    });
+
+    client.messages.on("message", (message, nodes) => {
+        const container = document.createElement("div");
+
+        const prefix = document.createElement("span");
+        prefix.innerText = `[Room #${i + 1}]: `;
+        container.appendChild(prefix);
+
+        for (const node of nodes) {
+            const element = document.createElement("span");
+            switch (node.type) {
+
+                case "entrance":
+                    element.innerText = node.text;
+                    element.style.color = "#018E42";
+                    break;
+                case "item":
+                    element.innerText = node.item.name;
+                    if (node.item.progression) {
+                        element.style.color = "#6B49B5";
+                    } else if (node.item.useful) {
+                        element.style.color = "#2635B8";
+                    } else if (node.item.trap) {
+                        element.style.color = "#D27138";
+                    } else {
+                        element.style.color = "#64A0E9";
+                    }
+                    break;
+                case "location":
+                    element.innerText = node.text;
+                    element.style.color = "#F7D002";
+                    break;
+                case "color":
+                    element.innerText = node.text;
+                    element.style.color = node.color;
+                    break;
+                case "player":
+                    element.innerText = node.player.alias;
+                    element.style.color = "#BF1A2F";
+                    break;
+                default:
+                case "text":
+                    element.innerText = node.text;
+                    break;
+            }
+
+            container.appendChild(element);
+        }
+
+        addChat(container);
+    });
+}
+
+function addChat(element) {
+    chat.appendChild(element);
+    chat.scrollTop = chat.scrollHeight;
+}
